96e600711eec087be672b85f809d8adc2ef92be4
Mohsin Ansari <ansarimohsin667@gmail.com>
2021-03-29 13:29:43 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    // let slickSlider = {
+    //     productCarousel: $('.product-slider'),
+    //     productCarouselNav: $('.slider-nav'),
+    //     relatedItemsCarousel: $(".related-slider"),
+
+    //     slick: function(el, slidesToShow, slidesToScroll, arrows, fade, asNavFor, dots, centerMode, focusOnSelect, infinite){
+            
+    //         el.slick({
+    //             slidesToShow: slidesToShow,
+    //             slidesToScroll: slidesToScroll,
+    //             arrows: arrows,
+    //             fade: fade,
+    //             asNavFor: asNavFor,
+    //             dots: dots,
+    //             centerMode: centerMode,
+    //             focusOnSelect: focusOnSelect,
+    //             infinite: infinite
+    //         });
+
+    //     },
+    //     init: function(){
+    //         slickSlider.slick(slickSlider.productCarousel,1,1,false,true,'.slider-nav',false,null,null,true);
+    //         slickSlider.slick(slickSlider.productCarouselNav,3,1,false,false,'.product-slider',false,true,true,true);
+    //         let slideW = 2.5;
+    //         let slidev = 2;
+    //         if ($(window).width() > 800) {
+    //             slideW = 5
+    //             slidev = 1
+    //         }
+    //         slickSlider.slick(slickSlider.relatedItemsCarousel, slideW, slidev, true, false, null, true, null, null, false);
+    //     }
+    // }
+
+    //Functions initialization
+    // slickSlider.init();
+
+});
