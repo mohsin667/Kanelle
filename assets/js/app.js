41c82d1cdd87df3964f3c1dbc679e421f70f148d
Mohsin Ansari <ansarimohsin667@gmail.com>
2021-03-29 20:09:55 +0530

    working on homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,36 @@
 $(document).ready(function(){
 
-    // let slickSlider = {
-    //     productCarousel: $('.product-slider'),
-    //     productCarouselNav: $('.slider-nav'),
-    //     relatedItemsCarousel: $(".related-slider"),
+    let slickSlider = {
+        productCarousel: $('.site__banner'),
 
-    //     slick: function(el, slidesToShow, slidesToScroll, arrows, fade, asNavFor, dots, centerMode, focusOnSelect, infinite){
+        slick: function(el, slidesToShow, slidesToScroll, arrows, fade, asNavFor, dots, centerMode, focusOnSelect, infinite){
             
-    //         el.slick({
-    //             slidesToShow: slidesToShow,
-    //             slidesToScroll: slidesToScroll,
-    //             arrows: arrows,
-    //             fade: fade,
-    //             asNavFor: asNavFor,
-    //             dots: dots,
-    //             centerMode: centerMode,
-    //             focusOnSelect: focusOnSelect,
-    //             infinite: infinite
-    //         });
+            el.slick({
+                slidesToShow: slidesToShow,
+                slidesToScroll: slidesToScroll,
+                arrows: arrows,
+                fade: fade,
+                asNavFor: asNavFor,
+                dots: dots,
+                centerMode: centerMode,
+                focusOnSelect: focusOnSelect,
+                infinite: infinite
+            });
 
-    //     },
-    //     init: function(){
-    //         slickSlider.slick(slickSlider.productCarousel,1,1,false,true,'.slider-nav',false,null,null,true);
-    //         slickSlider.slick(slickSlider.productCarouselNav,3,1,false,false,'.product-slider',false,true,true,true);
-    //         let slideW = 2.5;
-    //         let slidev = 2;
-    //         if ($(window).width() > 800) {
-    //             slideW = 5
-    //             slidev = 1
-    //         }
-    //         slickSlider.slick(slickSlider.relatedItemsCarousel, slideW, slidev, true, false, null, true, null, null, false);
-    //     }
-    // }
+        },
+        init: function(){
+            slickSlider.slick(slickSlider.productCarousel,1,1,false,false,null,true,null,null,true);
+            let slideW = 2.5;
+            let slidev = 2;
+            if ($(window).width() > 800) {
+                slideW = 5
+                slidev = 1
+            }
+        }
+    }
 
-    //Functions initialization
-    // slickSlider.init();
+    // Functions initialization
+    slickSlider.init();
     $(".trigger").on("click",function(){
         $(".left__menu").addClass("show");
         $("body").css("overflow-y","hidden")
@@ -58,5 +54,13 @@ $(document).ready(function(){
         e.stopPropagation();
         $(".navigation__right_dorp__menu").removeClass("visible")
         $(this).find(".navigation__right_dorp__menu").addClass("visible")
+    })
+    $(window).on("scroll",function(){
+        let scrollHeight = innerHeight / 4
+        if($(window).scrollTop()  > scrollHeight ) {
+            $("header").addClass("sticky");
+        }else {
+            $("header").removeClass("sticky");
+        }
     })
 });
