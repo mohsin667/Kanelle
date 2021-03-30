f778bb68481694b66878e5da0fd072502606c20d
Mohsin Ansari <ansarimohsin667@gmail.com>
2021-03-30 15:06:15 +0530

    working on contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,6 +35,53 @@ $(document).ready(function(){
             }
         }
     })
+    $(".contact__half_column form").on("submit",function(e){
+        e.preventDefault();
+        let res = true;
+        let userinput = $("#email").val();
+        let captcha = $("#captcha-field").val();
+         let pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
+        $(".form-field").each((ind,elm)=>{
+            $(elm).next("span").remove()
+            if($(elm).val() < 1) {
+                res = false;
+                $(elm).after("<span>All fields are mandatory</span>")
+            }else {
+                $(elm).next("span").remove()
+            }
+        })
+
+
+        if(userinput.length > 1 && !pattern.test(userinput))
+        {
+            res = false;
+            $("#email").next("span").remove()
+            $("#email").after("<span>Please provide a valid e-mail address</span>")
+        }
+        if(captcha.length > 1 && captcha !== $(".cap").val() ) {
+            res = false;
+            $("#captcha-field").next("span").remove()
+            $("#captcha-field").after("<span>Please provide enter a valid captcha code</span>")
+        }
+        return res
+    })
+    jQuery("#phoneInput, #Mobile-phone").on("keydown", function (e) {
+        if (e.which >= 48 && e.which <= 57) {
+            return true
+        }
+        if (e.which >= 186 && e.which <= 189) {
+        return true
+        }
+        if (e.which >= 8 && e.which <= 8) {
+            return true
+        }
+        if(e.which == 37 || e.which == 39) {
+            return true
+        }
+        else {
+            return false
+        }
+    })
     // if($("header.transparent").length < 1) {
     //     let headerHeight = $("header").height();
     //     $(".main__wrapper").css("margin-top",headerHeight+"px");
